c63219467049503eaa56e011e04a1f6cd7073a38
Ivan O <ioparin@pm.me>
2022-09-16 03:41:58 +0400

    fixing binary target

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "TensorFlowLite",
             url: "https://github.com/ivalx1s/swift-tensorflowlite-spm/releases/download/2.7.0/TensorFlowLite.xcframework.zip",
-            checksum: "af8ede7bf5bb506b06349fbbf7d0955b38a63784a2adcac1fcc2eae75d53f12b"
+            checksum: "9295b37a72d5628f6fca887a7eb5270d9b464313e8254631e8d01de82d76744c"
         )
     ]
 )
